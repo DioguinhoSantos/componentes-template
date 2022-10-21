84f0db347610531061035e960a61bbec93c0dc55
DioguinhoSantos <diiogosantos@live.com>
2022-10-21 16:55:24 -0300

    dioguinho

--- template/src/App.js
+++ template/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Garagem } from "./components/Garagem";
 import logo from "./img/logo.png";
 
 export default function App() {
   
-
   return (
     <div className="App">
-      
-     <h1>Aula - Componentes React</h1>
+      <h1>Aula - Componentes React</h1>
+      <Garagem/>
     </div>
   );
 }
---
+++ template/src/components/Garagem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Garagem(){
+    return (
+        <>
+        <h1>Garagem do Diogo</h1>
+        </>
+    )
+}
